b8b0210897d57bc215f983dc54247be9b964b070
Derek Shang <derekshang07@gmail.com>
2016-04-09 02:14:17 -0500

    login now uses templates

---
+++ login/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+
+def index(request):
+    template = loader.get_template('login/index.html')
+    return HttpResponse(template.render({}, request))
