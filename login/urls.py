9ed966f0177a90c47b20b2a221d3f6bd52c9413e
Derek Shang <derekshang07@gmail.com>
2016-04-10 01:01:09 -0500

    Finished basic login

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'login'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.sign_up, name='sign_up'),
+    url(r'^signin/', views.sign_in, name='sign_in',),
+    url(r'^user/(?P<username>)', views.temp, name='temp'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,9 +1,56 @@
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 
 def index(request):
-    template = loader.get_template('login/index.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'login/index.html', {})
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'login/index.html', {})
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+
+        if username and password and first_name and last_name and email:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+
+            return HttpResponseRedirect("user/%s/" % username)
+        else:
+            return render(request, 'login/index.html', {
+                'error_message': "Please fill out all blanks",
+            })
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        return render(request, 'login/signin.html', {})
+    else:
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+        except KeyError:
+            return render(request, 'login/signin.html', {
+                'error_message': "Signin information is incorrect",
+            })
+        else:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return HttpResponseRedirect("../user/%s/" % username)
+            else:
+                return render(request, 'login/signin.html', {
+                    'error_message': "Signin information is incorrect",
+            })
+
+
+def temp(reqeust, username):
+    return HttpResponse("good!")
